4ba7a6d679a076e5e01ac808cfebafa60260392f
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-02-06 18:53:08 +1100

    Upgrading fs2-core version to 2.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies
 
   lazy val faker = "com.github.javafaker" % "javafaker" % "1.0.1"
 
-  lazy val fs2 = "co.fs2" %% "fs2-core" % "2.2.1"
+  lazy val fs2 = "co.fs2" %% "fs2-core" % "2.2.2"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.0"
 
